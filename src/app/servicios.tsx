6f39805ec31b616aec6f4e72ebfface2b2117001
Guido Llaurado <guido.llaurado@gmail.com>
2024-02-23 10:40:24 -0300

    Normalizando gama de colores

--- src/app/servicios.tsx
+++ src/app/servicios.tsx
@@ -15,7 +15,7 @@ const ServiciosContainer = () => {
   return (
     
     
-    <div id="containerservicios" className={`relative mt-16 md:mt-40 bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 mr-8 ml-8 ${expanded ?  'h-auto' : 'h-auto overflow-hidden'}`}>
+    <div id="containerservicios" className={`relative mt-16 md:mt-40 bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 mr-8 ml-8 ${expanded ?  'h-auto' : 'h-auto overflow-hidden'}`}>
       <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
         <span className="flex text-white rounded-3xl p-5 text-4xl md:text-6xl">
           Nuestros servicios
@@ -54,48 +54,48 @@ const ServiciosContainer = () => {
         {expanded && (
           
           
-          <div className="flex grid grid-cols-1 md:grid-cols-1 items-center gap-5 md:gap-20 p-10 md:flex-row md:items-start md:justify-center expandable-container">
+          <div className="flex grid grid-cols-1 md:grid-cols-1 items-center gap-5 md:gap-20 md:p-10 md:flex-row md:items-start md:justify-center expandable-container">
             
 
-            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 ">
+            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 w-full">
             <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
 
-            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text">Soporte IT Online:</p>
+            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-emerald-500 text-transparent bg-clip-text">Soporte IT Online:</p>
             <ul>
               <li className="text-center p-5">Mediante un sistema de ticket vía Email mantenemos su empresa en correcto funcionamiento 24×7 los 365 días del año.</li>
             </ul></div></div>
             
-            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 ">
+            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 ">
             <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
 
 
-            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text ">Redes:</p>
+            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-emerald-500 text-transparent bg-clip-text ">Redes:</p>
             <ul>
               <li className="text-center p-5">Llevamos a cabo la implementación y diseño de sistemas de cableado estructurado, abarcando CAT5 / CAT6 y fibra óptica. Nos encargamos de instalar u organizar racks y realizar mejoras en su disposición.</li>
             </ul></div></div>
 
 
-            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 ">
+            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 ">
             <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
-            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text ">Videovigilancia y control de acceso:</p>
+            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-emerald-500 text-transparent bg-clip-text ">Videovigilancia y control de acceso:</p>
             <ul>
               <li className="text-center p-5">Instalamos cámaras IP y sistemas de CCTV así como también soluciones para el control de acceso.</li>
             </ul>
 
             </div></div>
 
-            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 ">
+            <div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 ">
             <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
-            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text ">Hardware y software empresarial:</p>
+            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-emerald-500 text-transparent bg-clip-text ">Hardware y software empresarial:</p>
             <ul>
               <li className="text-center p-5">
 Disponemos de una extensa red de colaboradores tecnológicos que nos capacitan para ofrecer las últimas innovaciones en software y hardware, asegurándonos de mantenernos al día con los requisitos específicos de su empresa o comercio.</li>
             </ul>
 </div></div>
 
-<div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-red-600 text-white rounded-l p-0.5 ">
+<div id="containerservicios" className= "relative bg-gradient-to-r from-blue-500 to-emerald-500 text-white rounded-l p-0.5 ">
             <div className="flex flex-col justify-center items-center p-2 bg-gray-900 rounded-l ">
-            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text ">Urgencias en el acto:</p>
+            <p className="text-3xl text-center bg-gradient-to-r from-blue-500 to-emerald-500 text-transparent bg-clip-text ">Urgencias en el acto:</p>
             <ul>
               <li className="text-center p-5" >Contratos con diferentes opciones de SLA (tiempo de respuesta y solución de problemas). Respuesta telefónica y contacto directo en caso de urgencia o denegación de servicio.</li>
             </ul>
