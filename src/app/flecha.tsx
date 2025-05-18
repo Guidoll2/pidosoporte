0cb32d1f333d0c3b671e2a25d9c9db647af9e423
Guidoll2 <guido.llaurado@gmail.com>
2025-05-18 16:09:03 -0300

    Public issue?

--- src/app/flecha.tsx
+++ src/app/flecha.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react';
+import { IoIosArrowUp } from 'react-icons/io';
 
 const IconoFlecha = () => {
   const [showArrow, setShowArrow] = useState(false);
@@ -20,13 +21,18 @@ const IconoFlecha = () => {
   }, []);
 
   return (
-   <a href='#navbar' > <img
-      src="flechaupsky400.png"
-      width={1000}
-      height={1000}
-      className={`fixed bottom-10 right-4 w-14 h-14 z-[100] ${showArrow ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8 transition-opacity ease-in duration-300'}`}
-      alt="Flecha"
-    /></a>
+    <a href="#navbar">
+      <IoIosArrowUp
+        size={56}
+        className={`fixed bottom-10 right-4 w-14 h-14 z-[100] bg-sky-400 rounded-full p-2 shadow-lg cursor-pointer transition-all ${
+          showArrow
+            ? 'opacity-100 translate-y-0'
+            : 'opacity-0 translate-y-8 transition-opacity ease-in duration-300'
+        }`}
+        color="#fff"
+        aria-label="Ir arriba"
+      />
+    </a>
   );
 };
 
